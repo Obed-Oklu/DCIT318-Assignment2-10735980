b3bc2232ec36900a34fc1800b0a4d62082ccc9a3
Obed-Oklu <oooklu@st.ug.edu.gh>
2024-07-06 02:18:36 -0700

    initial commit

---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace Interfaces
+{
+    class Program
+    {
+        interface IMovable
+        {
+            void Move();
+        }
+        class Car : IMovable
+        {
+            public void Move()
+            {
+                Console.WriteLine("Car is moving");
+            }
+        }
+
+        class Bicycle : IMovable
+        {
+            public void Move()
+            {
+                Console.WriteLine("Bicycle is moving");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            IMovable BMW = new Car();
+            IMovable BMX = new Bicycle();
+
+            BMW.Move();
+            BMX.Move();
+
+            Console.ReadKey();
+        }
+    }
+}
