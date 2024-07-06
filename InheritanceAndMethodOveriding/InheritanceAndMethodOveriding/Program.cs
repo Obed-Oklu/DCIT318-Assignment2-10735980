ec703c438b8ad22f40a6aec1c24ef4032212cee8
Obed-Oklu <oooklu@st.ug.edu.gh>
2024-07-06 00:56:12 -0700

    initial commit

---
+++ InheritanceAndMethodOveriding/InheritanceAndMethodOveriding/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace InheritanceAndMethodOveriding
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some Generic Sound");
+        }
+
+        class Dog : Animal
+        {
+            public override void MakeSound()
+            {
+                Console.WriteLine("Bark");
+            }
+        }
+        class Cat : Animal
+        {
+            public override void MakeSound()
+            {
+                Console.WriteLine("Meow");
+            }     
+        }
+
+
+        class program
+        {
+            static void Main(string[] args)
+            {
+                Animal LandAnimal = new Animal();
+                Dog Scooby = new Dog();
+                Cat Kitty = new Cat();
+
+                LandAnimal.MakeSound();
+                Scooby.MakeSound();
+                Kitty.MakeSound();
+                Console.ReadKey();
+            }
+        }
+
+    }
+    
+}
